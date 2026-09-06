fd23a711734d1ee5552c7934bcf54e7d1636b720
Sviatoslav Khramov <kent8352@gmail.com>
2026-09-06 13:52:50 +0300

    Replace JPA entities with embedded Mongo documents

--- src/main/java/io/github/khram0v/trainerworkload/model/TrainerWorkload.java
+++ src/main/java/io/github/khram0v/trainerworkload/model/TrainerWorkload.java
@@ -1,41 +1,39 @@
 package io.github.khram0v.trainerworkload.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
-@Entity
-@Table(name = "trainer_workloads")
+@Document(collection = "trainer_workloads")
+@CompoundIndex(name = "trainer_name_idx", def = "{'firstName' : 1, 'lastName' : 1}")
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TrainerWorkload {
 
     @Id
-    @Column(nullable = false, updatable = false)
+    @NotBlank
     private String username;
 
-    @Column(name = "first_name", nullable = false)
+    @NotBlank
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
+    @NotBlank
     private String lastName;
 
-    @Column(nullable = false)
     private boolean active;
 
-    @OneToMany(mappedBy = "trainerWorkload", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<WorkloadYear> years = new HashSet<>();
+    @Valid
+    private List<WorkloadYear> years = new ArrayList<>();
 
     public TrainerWorkload(String username, String firstName, String lastName, boolean active) {
         this.username = username;
