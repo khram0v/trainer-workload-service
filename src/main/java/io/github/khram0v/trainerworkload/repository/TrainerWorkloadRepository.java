b312f71937542e066f58da3c0d86c925702f34e8
Sviatoslav Khramov <kent8352@gmail.com>
2026-09-07 00:08:53 +0300

    Rewrite TrainerWorkloadRepository as MongoRepository

--- src/main/java/io/github/khram0v/trainerworkload/repository/TrainerWorkloadRepository.java
+++ src/main/java/io/github/khram0v/trainerworkload/repository/TrainerWorkloadRepository.java
@@ -1,19 +1,14 @@
 package io.github.khram0v.trainerworkload.repository;
 
 import io.github.khram0v.trainerworkload.model.TrainerWorkload;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface TrainerWorkloadRepository extends JpaRepository<TrainerWorkload, String> {
+public interface TrainerWorkloadRepository extends MongoRepository<TrainerWorkload, String> {
 
-    @Query("""
-            SELECT DISTINCT t FROM TrainerWorkload t
-            LEFT JOIN FETCH t.years y
-            LEFT JOIN FETCH y.months
-            WHERE t.username = :username
-            """)
-    Optional<TrainerWorkload> findByUsernameWithYearsAndMonths(@Param("username") String username);
+    Optional<TrainerWorkload> findByUsername(String username);
+
+    List<TrainerWorkload> findByFirstNameAndLastName(String firstName, String lastName);
 }
